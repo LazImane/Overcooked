7a4227ea55f915047a58d20b7765221c7b3d1369
LazImane <laziziimane8@gmail.com>
2025-10-03 16:31:08 +0200

    different stations, a first step to visuals

--- kitchen.gd
+++ kitchen.gd
@@ -1,39 +1,11 @@
 extends Node2D
 
-#4 stations: ingredients, chop, cook, serve
-var ingredient_station = {"type":"ingredients", "item":""}
-var chop_station = {"type":"chop", "item":""}
-var cook_station = {"type":"cook", "item":""}
-var serve_station = {"type":"serve", "item":""}
-
 func _ready():
-	print("Demo Kitchen ready! Press Enter to run one step of the pipeline.")
-
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		step_pipeline()
-
-func step_pipeline():
-	# 1) Ingredients station
-	if ingredient_station["item"] == "":
-		ingredient_station["item"] = "tomato"
-		print("[Ingredients] Spawned:", ingredient_station["item"])
-
-	# 2) Chopping station
-	if ingredient_station["item"] != "" and chop_station["item"] == "":
-		chop_station["item"] = "chopped_" + ingredient_station["item"]
-		print("[Chop] Chopped:", chop_station["item"])
-		ingredient_station["item"] = "" # item moved
-
-	# 3) Cooking station
-	if chop_station["item"] != "" and cook_station["item"] == "":
-		cook_station["item"] = "cooked_" + chop_station["item"].replace("chopped_","")
-		print("[Cook] Cooked:", cook_station["item"])
-		chop_station["item"] = "" # item moved
-
-	# 4) Serving station
-	if cook_station["item"] != "" and serve_station["item"] == "":
-		serve_station["item"] = cook_station["item"]
-		print("[Serve] Served:", serve_station["item"], "✅")
-		cook_station["item"] = "" # item moved
-		serve_station["item"] = "" # reset for next round
+	# Position all station nodes in a row
+	var x = 100
+	for station in get_children():
+		if station is Area2D:
+			station.position = Vector2(x, 200)
+			x += 200
+	print("Kitchen ready! Click stations to test.")
+	print("Stations: Ingredients -> Chop -> Cook -> Serve")
